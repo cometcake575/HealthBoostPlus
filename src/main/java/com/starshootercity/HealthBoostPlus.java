1507c9067cfff2c3062dc5a6abfd56adcce8d120
arunkapila <arunj.kapila@gmail.com>
2023-12-19 22:39:28 +0400

    Initial commit

---
+++ src/main/java/com/starshootercity/HealthBoostPlus.java
@@ -0,0 +1,10 @@
+package com.starshootercity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HealthBoostPlus extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
